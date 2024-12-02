fadf60d8cc31c7652a45f612fd27fa7840d44153
Erick Hunter <47735174+ErickHunter@users.noreply.github.com>
2024-12-02 05:58:31 +0200

    Update symlinks.sh
    Update with new and remove old tools

--- symlinks.sh
+++ symlinks.sh
@@ -6,6 +6,9 @@
 test -e "${HOME}/.zshrc" || ln -sfn "${HOME}/.files/zsh/zshrc" "${HOME}/.zshrc"
 test -e "${HOME}/.zsh" || ln -sfn "${HOME}/.files/zsh/" "${HOME}/.zsh"
 
+## P10k
+test -e "${HOME}/.p10k" || ln -sfn "${HOME}/.files/p10k" "${HOME}/.p10k"
+
 ## Vim
 test -e "${HOME}/.vimrc" || ln -sfn "${HOME}/.files/vim/vimrc" "${HOME}/.vimrc"
 test -e "${HOME}/.vim" || ln -sfn "${HOME}/.files/vim/" "${HOME}/.vim"
@@ -15,8 +18,3 @@ test -e "${HOME}/.gitconfig" || ln -sfn "${HOME}/.files/git/gitconfig" "${HOME}/
 test -e "${HOME}/.gitignore" || ln -sfn "${HOME}/.files/git/gitignore" "${HOME}/.gitignore"
 test -e "${HOME}/.gitattributes" || ln -sfn "${HOME}/.files/git/gitattributes" "${HOME}/.gitattributes"
 test -e "${HOME}/.gitmessage" || ln -sfn "${HOME}/.files/git/gitmessage" "${HOME}/.gitmessage"
-
-## Tmux
-test -e "${HOME}/.tmux.conf" || ln -sfn "${HOME}/.files/tmux/tmux.conf" "${HOME}/.tmux.conf"
-test -e "${HOME}/.tmux" || ln -sfn "${HOME}/.files/tmux/" "${HOME}/.tmux"
-
